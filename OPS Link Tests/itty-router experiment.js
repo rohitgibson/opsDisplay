3fe3cecfbeb36d324bbd60578422044f4ede8c75
Rohit Gibson <rgibso50@students.kennesaw.edu>
2022-09-09 16:49:20 -0400

    deleted accidental uploads

--- OPS Link Tests/itty-router experiment.js
+++
@@ -1,4 +0,0 @@
-import { Router } from 'itty-router'
-
-const router = Router() //Creates a new "Router" object, but apparently doesn't count as a "real" object, whatever the hell that means
-
--- OPS Link Tests/tito api test.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-tito_test = requests.get("https://api.tito.io/v3/hello", headers={"Authorization":"Token token=secret_2jyumm_qcZ23K_K3vzs5"}, verify=True)
-
-tito_content = tito_test.content
-print(tito_content)
